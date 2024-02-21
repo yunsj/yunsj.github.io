00f206636559db35b399ca674ad841611ed6b89b
yunsj <34021378+yunsj@users.noreply.github.com>
2024-02-21 13:42:29 +0900

    Update ble.js

--- etess/ble.js
+++ etess/ble.js
@@ -10,8 +10,8 @@ let deviceSerial = document.querySelector('#deviceSerial');
 let copyButton = document.querySelector('#copySerial');
 
 
-let service_uuid = '0000FFF0-0000-1000-8000-00805F9B34FB';
-let tx_uuid = '0000FFF2-0000-1000-8000-00805F9B34FB';
+let service_uuid = '00002902-0000-1000-8000-00805f9b34fb';
+let tx_uuid = '00002902-0000-1000-8000-00805f9b34fb';
 
 
 //let service_uuid = '6e400001-b5a3-f393-e0a9-e50e24dcca9e';
