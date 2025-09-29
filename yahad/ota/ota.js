d8029d5e3a0da23e3d601048597d8d191352ebba
yunsj <34021378+yunsj@users.noreply.github.com>
2025-09-29 23:23:31 +0900

    Update ota.js

--- yahad/ota/ota.js
+++ yahad/ota/ota.js
@@ -87,7 +87,7 @@ async function handleNotifications(event){
 
   switch (value.getUint8(0)){
     case 0xAA: //transfer mode
-    textAlert.textContent += "mode: " + (value.getUint8(1)==1);
+    textAlert.textContent += " mode: " + (value.getUint8(1)==1);
       if (value.getUint8(1) == 1){
         for (let x = 0; x < fileParts; x++){
           let pr = Math.trunc((x/fileParts)*100) + '%';
@@ -96,7 +96,7 @@ async function handleNotifications(event){
           await sendPart(x);
         }
       } else {
-        await sendPart(0);
+        //await sendPart(0);
       }
 
     break;
